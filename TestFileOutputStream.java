83124627cf6f7d1a6f18da7d51a2b96a31c2354f
JDEGUZMAN@win-wks300774 <dgjohnadrian@gmail.com>
2022-05-10 14:26:16 +0800

    This is my first GIT commit.

---
+++ TestFileOutputStream.java
@@ -0,0 +1,35 @@
+package org.ssglobal.training.codes.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TestFileOutputStream {
+
+	public static void main(String[] args) {
+		File file = new File("./src/config/poem.txt");
+		Scanner scan = new Scanner(System.in);
+
+		try (FileOutputStream fos = new FileOutputStream(file);) {
+			// Composition Design Pattern
+			while (true) {
+				System.out.println("Enter a Message: ");
+				String message = scan.nextLine();
+				String messageLine = String.join("", message, "\n"); // fos does not include line feed
+				byte[] msgBytes = messageLine.getBytes();
+				if (message.length() == 0) {
+					break;
+				}
+				fos.write(msgBytes);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		scan.close();
+	}
+}
